b74dd027c96bc73acda235e52017a0b97c54381a
DamoLu <yanyanglu@foxmail.com>
2019-05-25 16:24:37 +0800

    push project

---
+++ src/main/java/com/example/my/qihualu/QihualuApplication.java
@@ -0,0 +1,13 @@
+package com.example.my.qihualu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QihualuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QihualuApplication.class, args);
+    }
+
+}
